49f97210681212ca857b0fb2c398594d82916461
Martin-Rouault <martin.rlt24@gmail.com>
2025-04-02 17:03:05 +0200

    immer integration

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -3,34 +3,30 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
     todos: [],
 };
-// Création du slice Redux pour les tâches
+
+// Création du slice Redux pour les tâches avec Immer intégré
 const todoSlice = createSlice({
     name: "todos",
     initialState,
     reducers: {
         // Action pour ajouter une tâche
         addTodo: (state, action) => {
-            return {
-                ...state, // Copie les propriétés de l'état actuel
-                todos: [
-                    ...state.todos, // Copie les tâches existantes
-                    { id: Date.now(), texte: action.payload, completed: false }, // Ajoute la tâche
-                ],
-            };
+            state.todos.push({
+                id: Date.now(),
+                texte: action.payload,
+                completed: false,
+            });
         },
         // Action pour marquer une tâche comme terminée
         toggleTodo: (state, action) => {
-            return {
-                ...state, // Copie les propriétés de l'état actuel
-                todos: state.todos.map((todo) =>
-                    todo.id === action.payload
-                        ? { ...todo, completed: !todo.completed } // Copie la tâche et inverse 'completed'
-                        : todo
-                ),
-            };
+            const todo = state.todos.find((todo) => todo.id === action.payload);
+            if (todo) {
+                todo.completed = !todo.completed;
+            }
         },
     },
 });
+
 // Exportation des actions et du reducer
 export const { addTodo, toggleTodo } = todoSlice.actions;
 export default todoSlice.reducer;
